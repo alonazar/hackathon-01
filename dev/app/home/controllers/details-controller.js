bd97c079da115c2ccb72100bb280ffe3617412e1
Bhodgson <bhodgson@uvic.ca>
2016-08-12 14:43:36 -0700

    Because reasons

--- dev/app/home/controllers/details-controller.js
+++ dev/app/home/controllers/details-controller.js
@@ -4,7 +4,7 @@ angular.module('app.home').controller('DetailsController', function ($scope, $lo
     console.log("details-controller: result id=%s",$stateParams.id);
     
     VenueModel.get({id: $stateParams.id}, function(res) {
-        $scope.venue = res[0];
+        $scope.venue = res[1];
         console.log("Got venue with coordinates: %s, %s",$scope.venue.loc.lat, $scope.venue.loc.lon)
 });
 });
